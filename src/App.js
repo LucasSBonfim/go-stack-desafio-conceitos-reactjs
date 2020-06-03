869ae9725d60a4e6a967714e7ffa1963d7a2c87f
Lucas Simas <lu_bonfim@hotmail.com>
2020-06-03 00:54:48 -0300

    Primeiro comitt do desafio conceitos reactJS ✅

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+      api.get('/repositories').then(response => {
+          console.log(response)
+          setProjects(response.data);
+      })
+  }, [])
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', 
+    {
+        title: `go-stack-desafio-conceitos-node ${Date.now()}`,
+        owner: "Lucas Simas",
+        techs: ["React", "ReactJS", "nodeJS", "typeScript"]
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository(id) { 
+    const response = await api.delete(`/repositories/${id}`);
+    setProjects(projects.filter((response) => {
+      return response.id !== id;
+    }));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {projects.map(project => 
+            <li key={project.id}>
+              {project.title}
+              <span style={{padding: 20 + 'px'}} > 
+                {project.techs.map(techs => <h4 key={techs}> {techs}</h4>)}
+              </span>
+              <button onClick={() => handleRemoveRepository(project.id)}>
+                Remover
+              </button>
+          </li>)}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
